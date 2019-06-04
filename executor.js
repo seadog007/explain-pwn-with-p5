2ade350e283296c09f2a6d52f2e03013cb062ec3
Jasper Yu <007seadog@gmail.com>
2019-06-04 10:13:08 +0900

    Added executor and simple sys_read, sys_write example

---
+++ executor.js
@@ -0,0 +1,23 @@
+class Executor {
+  constructor(reg, page, io) {
+    this.reg = reg;
+    this.page = page;
+    this.io = io;
+  }
+  next() {
+    if (this.reg.STOP){
+      return this.reg.RET;
+    }
+    console.log('running', this.reg.get('rip').toString(), this.page.get(this.reg.get('rip')).toString(16));
+    let ins_set = [];
+    let rip = this.reg.get('rip').toString();
+
+    for (let i = BigInt(0); i < 16; i++) {
+      ins_set.push(parseInt(this.page.get(this.reg.get('rip') + i).toString()))
+    }
+
+    let parser = new Parser(ins_set);
+    let used = parser.do_it(reg, page, io);
+    reg.set('rip', this.reg.get('rip') + BigInt(used))
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,5 +1,10 @@
 let reg = new Register();
 let page = new Page();
+let stdin = new FD('stdin', {'read': prompt.bind(window)});
+let stdout = new FD('stdout', {'write': console.log});
+let stderr = new FD('stderr', {'write': alert.bind(window)});
+let io = new IO([stdin, stdout, stderr]);
+var executor = new Executor(reg, page, io);
 
 function preload() {
   fontRegular = loadFont('Menlo-Regular.ttf');
@@ -12,10 +17,16 @@ function setup() {
   let program = page.new_segment('0x4004b0', '0x100', 'r-xp', 'program');
   let stack = page.new_segment('0x7fffffff0000', '0x1000', 'rw-p', 'stack0');
 
+  page.map('0x4004b2', '\x90\x55\x90\x59\x90\x0f\x05\xb0\x01\xbf\x02\x00\x00\x00\x0f\x05');
+
   reg.set('rbp', '0x7fffffff0200');
   reg.set('rsp', '0x7fffffff0200');
   reg.set('rip', '0x4004b2');
-
+  reg.set('rax', 0x0);
+  reg.set('rdi', 0x0);
+  page.map('0x7fffffff0250', 'ABCDEFG');
+  reg.set('rdx', 0x20);
+  reg.set('rsi', 0x7fffffff0250);
 }
 
 function draw() {
@@ -25,4 +36,6 @@ function draw() {
   hex_dump(50, 200, page, reg.get('rsp'), '0x80');
   hex_dump(100, 400, page, '0x4004b0', '0x60');
   vmmap_box(550, 200, page);
+
+  executor.next();
 }
